b83fe230b0dbc6f3ad2ed3cb70b6264f2d35bde3
Nguyễn Trung Hiếu <145263187+nghieee@users.noreply.github.com>
2024-09-29 00:40:27 +0700

    uppdate

--- obj/Debug/net6.0/DoAnPhanMem.AssemblyInfo.cs
+++ obj/Debug/net6.0/DoAnPhanMem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnPhanMem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1b735946681ba44f38b2fa92bb65486488a4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2366fa0c6e7042b0aa686e37a602d56cfb412a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnPhanMem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnPhanMem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
